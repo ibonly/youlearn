01292839342b269a6ec4acc76c2c48d265c11fcd
andela-iadeniyi <ibonly01@gmail.com>
2016-01-08 22:13:59 +0100

    (Feature) Update video layout

--- resources/views/contents/videos.blade.php
+++ resources/views/contents/videos.blade.php
@@ -6,13 +6,22 @@
 
     @forelse($videos as $video)
 
-        <div id="show-video">
-            <a href="/video/{{ $video->slug }}/edit" id="EDIT" id="edit-link"><i class="fa fa-pencil-square"></i></a>
-
-            <img src="http://i1.ytimg.com/vi/{{ $video->url }}/hqdefault.jpg" width="100%" class="img-responsive" alt="">
-            <div id="title" class="truncate">{{ $video->title }}</div>
-            <a class="waves-effect waves-light btn" href="/play/{{ $video->slug }}"><i class="material-icons left">visibility</i>view</a><br />
+    <div id="show-video" class="s12">
+        <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img src="http://i1.ytimg.com/vi/{{ $video->url }}/hqdefault.jpg" width="100%" class="img-responsive" alt="">
+            </div>
+            <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4 truncate" id="title">{{ $video->title }}</span>
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Video Title<i class="material-icons right">close</i></span>
+                <p>{{ $video->title }}.</p>
+                <a class="waves-effect waves-light btn left" href="/play/{{ $video->slug }}" id="view"><i class="material-icons left">visibility</i></a>
+                <a class="waves-effect waves-light btn right" href="/video/{{ $video->slug }}/edit"><i class="fa fa-pencil-square"></i></a>
+            </div>
         </div>
+    </div>
 
     @empty
 
